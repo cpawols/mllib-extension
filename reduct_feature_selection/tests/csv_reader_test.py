ff88f7da207ac8d3ead870f6e24a26d05cb6f706
pawols <cpawols@gmail.com>
2015-11-02 20:43:25 +0100

    Adding path do global folder - description in mail

--- reduct_feature_selection/tests/csv_reader_test.py
+++ reduct_feature_selection/tests/csv_reader_test.py
@@ -1,4 +1,5 @@
 from numpy.testing import assert_array_equal
+import os
 from unittest import TestCase
 from reduct_feature_selection.commons.cvs_reader import CSVReader
 import numpy as np
@@ -6,11 +7,12 @@ import numpy as np
 
 class TestCsvReaderTest(TestCase):
     def test_read_csv_file(self):
-        matrix = CSVReader.read_csv("/home/krzysztof/mgr/mllib-extension/data/csv_test_file.csv")
+        matrix = CSVReader.read_csv(os.path.expanduser('~') + "/.mllib-extension/data/csv_test_file.csv")
+
         expected_output = np.array([[1, 2, 3, 4], [5, 5, 4, 5], [1, 1, 4, 5]])
         assert_array_equal(matrix, expected_output)
 
     def test_read_csv_float_value(self):
-        matrix = CSVReader.read_csv("/home/krzysztof/mgr/mllib-extension/data/csv_test_float_numbers.csv")
+        matrix = CSVReader.read_csv(os.path.expanduser('~') + "/.mllib-extension/data/csv_test_float_numbers.csv")
         expected_output = np.array([[1.1, 3.1, 3.3], [1.2, 0.9, 3.111]])
         assert_array_equal(matrix, expected_output)
