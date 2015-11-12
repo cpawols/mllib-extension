42850b3aaec93d6b3a058f49a2f8b3d76138efa0
Krzysztof <krisun17@gmail.com>
2015-11-13 00:13:52 +0100

    rename files and removing sort examples

---
+++ reduct_feature_selection/tests/eav_test.py
@@ -0,0 +1,66 @@
+"""
+Test for EavConverter class
+"""
+from unittest import TestCase
+import numpy as np
+from reduct_feature_selection.commons.eav import Eav
+
+__author__ = 'krzysztof'
+
+
+class TestEav(TestCase):
+    def test_to_eav_convert(self):
+        array_example = np.array([(0, 1), (4, 5)], dtype=[('x', int), ('y', float)])
+        expected_output = [(0, 'x', 0), (0, 'y', 1), (1, 'x', 4), (1, 'y', 5)]
+        real_output = Eav.from_array(array_example).eav
+        self.assertEqual(expected_output, real_output,
+                         msg="wrong conversion from array to eav, tables are not equal")
+
+    def test_to_array_convert(self):
+        eav_example = [(0, 'x', 0), (0, 'y', 1), (1, 'x', 4), (1, 'y', 5)]
+        expected_output = np.array([(0, 1), (4, 5)], dtype=[('x', float), ('y', float)])
+        real_output = Eav(eav_example).convert_to_array()
+        self.assertTrue(np.array_equal(expected_output, real_output),
+                        msg="wrong conversion from eav to array, tables are not equal")
+
+    def test_conversion_involution(self):
+        eav_example = [(0, 'x', 0), (0, 'y', 1), (1, 'x', 4), (1, 'y', 5)]
+        real_output = Eav.from_array(Eav(eav_example).convert_to_array()).eav
+        self.assertEqual(eav_example, real_output,
+                         msg="error: conversions must be involution")
+
+    def test_empty_conversion(self):
+        eav_example = []
+        expected_output = np.array([])
+        real_output = Eav(eav_example).convert_to_array()
+        self.assertTrue(np.array_equal(expected_output, real_output),
+                        msg="wrong conversion from empty eav")
+
+    def test_wrong_array_format_exception(self):
+        with self.assertRaises(TypeError, msg="don't raise wrong format exception"):
+            array_example = np.array([[0, 1], [4, 5]])
+            Eav.from_array(array_example)
+
+    def test_update_index(self):
+        array_example = np.array([(0, 1), (4, 5)], dtype=[('x', int), ('y', float)])
+        expected_obj_index = {0: [0, 1], 1: [2, 3]}
+        expected_attr_index = {'x': [0, 2], 'y': [1, 3]}
+        eav = Eav.from_array(array_example)
+        self.assertEqual(expected_obj_index, eav.update_index(0))
+        self.assertEqual(expected_attr_index, eav.update_index(1))
+
+    def test_sort(self):
+        eav_example = [(0, 'x', 5), (0, 'y', 8), (1, 'x', 4), (1, 'y', 5), (1, 'z', 2), (0, 'z', 3)]
+        expected_output = [(1, 'x', 4), (0, 'x', 5), (1, 'y', 5), (0, 'y', 8), (1, 'z', 2), (0, 'z', 3)]
+        sort_eav = Eav(eav_example)
+        sort_eav.sort()
+        self.assertEqual(expected_output, sort_eav.eav)
+
+    def test_merge_sort(self):
+        eav_example = [(0, 'x', 5), (0, 'y', 8), (1, 'x', 4), (1, 'y', 5), (1, 'z', 2), (0, 'z', 3)]
+        expected_output = [(1, 'x', 4), (0, 'x', 5), (1, 'y', 5), (0, 'y', 8), (1, 'z', 2), (0, 'z', 3)]
+        sort_eav = Eav(eav_example)
+        sort_eav.merge_sort()
+        self.assertEqual(expected_output, sort_eav.eav)
+
+
