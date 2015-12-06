848d82fc321f203199a062881f0bdc331f87dbb5
Krzysztof <krisun17@gmail.com>
2015-12-06 17:58:39 +0100

    adding computing rules from Tianrui Li paper

--- reduct_feature_selection/rule_classifier/compute_ind_class.py
+++ reduct_feature_selection/rule_classifier/compute_ind_class.py
@@ -1,4 +1,3 @@
-import numpy as np
 from settings import sc
 
 __author__ = 'krzysztof'
@@ -10,15 +9,44 @@ def compute_ind_class(decision_system, attr_set, subtable_num):
     :type decision_system: np.array
     :param attr_set:
     :type attr_set: list (numbers of columns)
-    :return: list of tuples (rule, rule_support)
+    :return: dict of list of tuples {"partitions" : [(partition, support)], "rule" : [(rule, support)]}
     """
     rows = decision_system.shape[0]
     cols = decision_system.shape[1]
-    attrs = attr_set.append(cols - 1)  # adding decision
-    subtable = decision_system[:rows, attrs]
-    subtable_as_list = list([tuple(row) for row in subtable])   # tu jest coś z typem atomowym w tupli (powinien być prosty chyba a jest numpy'owy)
+    subtable = decision_system[:rows, attr_set]
+    attr_set.append(cols - 1)  # adding decision
+    dec_subtable = decision_system[:rows, attr_set]
+
+    subtable_as_list = list([tuple(row) for row in subtable])
+    dec_subtable_as_list = list([tuple(row) for row in dec_subtable])
+
     num_chunks = subtable_num
+
     rdd_subtable = sc.parallelize(subtable_as_list, num_chunks)
-    rules = rdd_subtable.mapPartitions(lambda x: (x, 1)) \
-        .reduceByKey(lambda x, y: x + y).reduce(lambda x, y: x.extend(y)) #reducyBykey się wykrzacza, do reducera trzeba dopisać funkcję merge'ująca
-    return rules
+    rdd_dec_subtable = sc.parallelize(dec_subtable_as_list, num_chunks)
+
+    partitions = rdd_subtable.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y).sortByKey().collect()
+    rules = rdd_dec_subtable.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y).sortByKey().collect()
+
+    return {"partitions": partitions, "rules": rules}
+
+
+def compute_rules(decision_system, attr_set, treshold, subtable_num):
+    """
+    :param decision_system: (last column is decision column)
+    :type decision_system: np.array
+    :param attr_set:
+    :type attr_set: list (numbers of columns)
+    :param treshold: lowerbound treshold for accuracy of rules
+    :return: list of rules (last element in rule is decision)
+    """
+    rule_candidates = compute_ind_class(decision_system, attr_set, subtable_num)
+    rule_set = []
+
+    for rule in rule_candidates["partitions"]:
+        good_rules = filter(lambda r: r[0][:-1] == rule[0]
+                                      and float(r[1]) / float(rule[1]) > treshold, rule_candidates["rules"])
+        rule_set.append(map(lambda x: x[0], good_rules))
+    rule_set = reduce(lambda x, y: x + y, rule_set)
+
+    return rule_set
--- reduct_feature_selection/tests/rule_classifier_test.py
+++ reduct_feature_selection/tests/rule_classifier_test.py
@@ -1,12 +1,29 @@
 from unittest import TestCase
 import numpy as np
 
-from reduct_feature_selection.rule_classifier.compute_ind_class import compute_ind_class
+from reduct_feature_selection.rule_classifier.compute_ind_class import compute_ind_class, compute_rules
 
 
 class TestRuleClassifier(TestCase):
     def test_compute_ind_class(self):
-        array = np.array([[1, 2, 3, 4, 1], [6, 7, 3, 4, 0], [2, 3, 3, 4, 1], [1, 2, 2, 3, 0]])
-        expected_result = [((3, 4, 1), 2), ((3, 4, 0), 1), ((2, 3, 0), 1)]
+        array = np.array([[1, 2, 3, 4, 1], [6, 7, 3, 4, 0], [2, 3, 3, 4, 1], [1, 2, 2, 3, 0],
+                          [1, 2, 2, 3, 0], [1, 2, 2, 3, 0], [1, 2, 2, 3, 0], [6, 7, 3, 4, 0],
+                          [1, 2, 2, 3, 1], [1, 2, 2, 3, 1]])
+        expected_result = {"partitions": [((2, 3), 6), ((3, 4), 4)],
+                           "rules": [((2, 3, 0), 4), ((2, 3, 1), 2), ((3, 4, 0), 2), ((3, 4, 1), 2)]}
         result = compute_ind_class(array, [2, 3], 2)
         self.assertEqual(expected_result, result)
+
+    def test_compute_rules(self):
+        array1 = np.array([[1, 2, 3, 4, 1], [6, 7, 3, 4, 0], [2, 3, 3, 4, 1], [1, 2, 2, 3, 0],
+                           [1, 2, 2, 3, 0], [1, 2, 2, 3, 0], [1, 2, 2, 3, 0], [6, 7, 3, 4, 0],
+                           [1, 2, 2, 3, 1], [1, 2, 2, 3, 1]])
+        array2 = np.array([[1, 2, 3, 4, 1], [6, 7, 3, 4, 0], [2, 3, 3, 4, 0], [1, 2, 2, 3, 0],
+                           [1, 2, 2, 3, 0], [1, 2, 2, 3, 0], [1, 2, 2, 3, 0], [6, 7, 3, 4, 0],
+                           [1, 2, 2, 3, 1], [1, 2, 2, 3, 1]])
+        expected_result1 = [(2, 3, 0)]
+        expected_result2 = [(2, 3, 0), (3, 4, 0)]
+        result1 = compute_rules(array1, [2, 3], 0.5, 2)
+        result2 = compute_rules(array2, [2, 3], 0.5, 2)
+        self.assertEqual(expected_result1, result1)
+        self.assertEqual(expected_result2, result2)
